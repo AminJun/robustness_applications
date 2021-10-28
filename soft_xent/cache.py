c0f4a0e541fa439791e8836d55a0d9873162acc4
AminJun <mohammad.amin.ghiasi@gmail.com>
2021-10-28 15:20:11 -0400

    See shape and size of mean

--- soft_xent/cache.py
+++ soft_xent/cache.py
@@ -15,7 +15,7 @@ class CachedData(SoftLabelData):
 
     def _run_or_load(self, callable_func, *args, **kwargs):
         name = f'{callable_func.__name__}.pt'
-        par = os.path.join(self._path, 'checkpoints', 'auto_save')
+        par = os.path.join(self._path, 'checkpoints', 'auto_save', self.__class__.__name__)
         os.makedirs(par, exist_ok=True)
         path = os.path.join(par, name)
         if os.path.exists(path):
@@ -32,18 +32,49 @@ class CachedData(SoftLabelData):
         self._path = cache_path
 
     @torch.no_grad()
-    def cache(self, model: nn.Module, train: DataLoader, eval: DataLoader):
+    def cache(self, model: nn.Module, train: DataLoader, test: DataLoader):
         t_x, t_y = self._run_or_load(self.cache_train, model=model, train=train)
-        e_x, e_y = self._run_or_load(self.cache_eval, model=model, eval=eval)
+        e_x, e_y = self._run_or_load(self.cache_test, model=model, eval=test)
+        classes = t_y.unique()
+        self.n = len(classes)
+        self.one_t = self._run_or_load(self.cache_one_train, predictions=t_x, targets=t_y)
+        self.one_e = self._run_or_load(self.cache_one_test, predictions=e_x, targets=e_y)
+        self.mean_t = self._run_or_load(self.cache_mean_t, predictions=t_x, targets=t_y)
         pdb.set_trace()
+        self.mean_e = self._run_or_load(self.cache_mean_e, predictions=e_x, targets=e_y)
+        # self.dist_e = self._run_or_load()
+
+    @torch.no_grad()
+    def cache_dist(self, p: torch.tensor, t: torch.tensor) -> torch.tensor:
+        pass
+
+    @torch.no_grad()
+    def cache_one_train(self, predictions: torch.tensor, targets: torch.tensor) -> torch.tensor:
+        return torch.cat([predictions[targets == i][0] for i in range(self.n)])
+
+    @torch.no_grad()
+    def cache_one_test(self, predictions: torch.tensor, targets: torch.tensor) -> torch.tensor:
+        return torch.cat([predictions[targets == i][0] for i in range(self.n)])
+
+    @torch.no_grad()
+    def cache_mean_t(self, predictions: torch.tensor, targets: torch.tensor) -> torch.tensor:
+        return self.cache_mean(predictions, targets)
+
+    @torch.no_grad()
+    def cache_mean_e(self, predictions: torch.tensor, targets: torch.tensor) -> torch.tensor:
+        return self.cache_mean(predictions, targets)
+
+    @torch.no_grad()
+    def cache_mean(self, predictions: torch.tensor, targets: torch.tensor) -> torch.tensor:
+        return torch.cat([predictions[targets == i].mean(dim=0) for i in range(self.n)])
 
     @torch.no_grad()
     def cache_train(self, model: nn.Module, train: DataLoader) -> (torch.tensor, torch.tensor):
         return self.cache_loader(model, train)
 
     @torch.no_grad()
-    def cache_eval(self, model: nn.Module, eval: DataLoader) -> (torch.tensor, torch.tensor):
-        return self.cache_loader(model, eval)
+    def cache_test(self, model: nn.Module, test: DataLoader) -> (torch.tensor, torch.tensor):
+        return self.cache_loader(model, test)
 
     @torch.no_grad()
     def cache_loader(self, model: nn.Module, loader: DataLoader) -> (torch.tensor, torch.tensor):
