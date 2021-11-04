77340fa6d0a1e1ba92e3312f5cd784d12962e285
AminJun <mohammad.amin.ghiasi@gmail.com>
2021-11-04 19:00:59 -0400

    Save

--- datasets/utils/class_sorted.py
+++ datasets/utils/class_sorted.py
@@ -48,7 +48,7 @@ class ClassSortedFactory:
         _, n_classes = dataset[len(dataset) - 1]
         n_classes = n_classes + 1
         out = {k: (ClassSortedFactory._lb(dataset, k), ClassSortedFactory._ub(dataset, k)) for k in
-               tqdm(range(998, n_classes))}
+               tqdm(range(n_classes))}
         return out
 
     @staticmethod
