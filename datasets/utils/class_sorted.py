f8b04ee062588fe24984e5e4aca945c8f5881951
AminJun <mohammad.amin.ghiasi@gmail.com>
2021-11-04 18:21:35 -0400

    Save Let's try class sorted

--- datasets/utils/class_sorted.py
+++ datasets/utils/class_sorted.py
@@ -36,6 +36,37 @@ class ClassSortedFactory:
         return {k: ClassSortedFactory.zip(v) for k, v in out.items()}
 
     @staticmethod
+    def binary_search_cache(dataset: Dataset):
+        _, n_classes = dataset[len(dataset) - 1]
+        n_classes = n_classes.item() + 1
+        out = {k: (ClassSortedFactory._lb(dataset, k), ClassSortedFactory._up(dataset, k)) for k in range(n_classes)}
+
+    @staticmethod
+    def _get_label(dataset: Dataset, i: int) -> int:
+        return dataset[i][1].item()
+
+    @staticmethod
+    def _lb(dataset: Dataset, label: int):
+        f, e = 0, len(dataset)
+        while f < e - 1:
+            m = (f + e) // 2
+            _, y = ClassSortedFactory._get_label(dataset, m)
+            if y < label:
+                f = m
+            else:
+                e = m
+        _, y = dataset[f]
+        assert y == label
+        if f > 0:
+            _, y = dataset[f - 1]
+            assert y != label
+        return f
+
+    @staticmethod
+    def _ub(dataset: Dataset, label: int):
+        return ClassSortedFactory._lb(dataset, label + 1)
+
+    @staticmethod
     def zip(indices: list) -> []:
         if len(indices) == 0:
             return []
