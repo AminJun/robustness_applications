abc645d5c26b4caa05d54dd095806ecaa476311b
AminJun <mohammad.amin.ghiasi@gmail.com>
2021-11-04 20:17:04 -0400

    See if everything works

--- test_cache_1000.py
+++ test_cache_1000.py
@@ -1,3 +1,5 @@
+import pdb
+
 import torch
 from torch.utils.data import DataLoader, Subset
 import torchvision as tv
@@ -27,15 +29,19 @@ def main():
             samples.append(inits(c, True))
     tv.utils.save_image(torch.cat(samples), 'sampled8.png', nrow=len(classes))
 
-    factory = ClassSortedFactory(image_net, False, True)
-    indices = [factory(c) for c in classes]
-    indices = [i for li in indices for i in li]
-    Subset(image_net.eval(), indices)
+    factory_e = ClassSortedFactory(image_net, False, True)
+    factory_t = ClassSortedFactory(image_net, True, True)
+    eval = Subset(image_net.eval(), [i for c in classes for i in factory_e(c)])
+    train = Subset(image_net.eval(), [i for c in classes for i in factory_t(c)])
+    eval = DataLoader(eval, batch_size, num_workers=4)
+    train = DataLoader(train, batch_size, num_workers=4)
+
     labels_cache = IN1000RobustLabels('.')
-    # labels_cache.cache(model, image)
+    labels_cache.cache(model, train, eval)
     labels = []
-    # for i in range(7):
-    #     labels.append(labels_cache.)
+    for i in range(7):
+        labels.append(labels_cache(mode=i))
+    pdb.set_trace()
 
 
 if __name__ == '__main__':
