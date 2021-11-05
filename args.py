2796a833aa3d86d3dba3145c5aa36277764697e7
AminJun <mohammad.amin.ghiasi@gmail.com>
2021-11-04 23:11:17 -0400

    Save

--- args.py
+++ args.py
@@ -40,7 +40,7 @@ class Args:
         parser.add_argument('-t', '--toml', type=str, help='Path to toml file that contains the default configs')
         parser.add_argument('-x', '--experiment', type=str, help='Experiment name')
 
-        parser.add_argument('-m', '--method', type=str, default=None,
+        parser.add_argument('-m', '--method', type=int, default=0,
                             help='Inversion method, differs for every experiment')
         parser.add_argument('--seed', type=int, default=6247423, help='Random seed')
         return parser
