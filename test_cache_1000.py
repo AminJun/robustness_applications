d0791101f46187e030e6510d4eabfd1e50973335
AminJun <mohammad.amin.ghiasi@gmail.com>
2021-11-04 23:14:56 -0400

    save

--- test_cache_1000.py
+++ test_cache_1000.py
@@ -97,7 +97,7 @@ def main():
     dataset_function = getattr(datasets, DATA)
     dataset = dataset_function(DATA_PATH_DICT[DATA])
 
-    model = AttackerModel(model, dataset)
+    model = AttackerModel(model, dataset).cuda()
 
     for i in tqdm(classes):
         target_class = i * torch.ones((BATCH_SIZE,)).long().cuda()
